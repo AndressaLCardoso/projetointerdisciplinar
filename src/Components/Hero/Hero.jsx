988c8e5861cd21351fdf655402feaa3744910d2e
Andressa Lira <andressa_lira@outlook.com>
2023-10-22 15:37:53 -0300

    Editando pg inicial

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,24 +1,21 @@
 import React from 'react'
 import './Hero.css'
-import hand_icon from '../Assets/hand_icon.png'
+//import hand_icon from '../Assets/hand_icon.png'
 import arrow_icon from '../Assets/arrow.png'
 import hero_image from '../Assets/hero_image.png'
+
 
 export const Hero = () => {
   return (
     <div className='hero'>
         <div className='hero-left'>
-            <h2>NEW ARRIVALS ONLY</h2>
+            
             <div>
-                <div className='hero-hand-icon'>
-                    <p>new</p>
-                    <img src={hand_icon} alt='' />
-                </div>
-                <p>collections</p>
-                <p>for everyone</p>
+                <p>Seu mercado,</p>
+                <p>sem sair de casa</p>
             </div>
             <div className='hero-latest-btn'>
-                <div>Latest Collection</div>
+                <div>Peça agora</div>
                 <img src={arrow_icon} alt='' />
             </div>
         </div>
